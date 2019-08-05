ba4446153cab0a8515e86b7a1525e5e1434bec62
KhoaMilan97 <42708093+KhoaMilan97@users.noreply.github.com>
2019-08-05 16:17:55 +0700

    Updates

---
+++ precache-manifest.ad8a1cfefda9d085a480d73b55f9d698.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c0f7e88d31515d9663eedf1e64b4810",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f783eb8fa1ae83867bab",
+    "url": "/monsters-rolodex/static/css/main.a9de5907.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "f783eb8fa1ae83867bab",
+    "url": "/monsters-rolodex/static/js/main.19c9b36f.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
